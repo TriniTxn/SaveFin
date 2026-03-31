ceb81370dd50a36e6645711999990d1b16e95983
Giovanni dos Santos Gomes <giovanni.sgomes@gmail.com>
2026-03-31 03:15:01 -0300

    adiciona a arquitetura do projeto

---
+++ src/main/java/com/saveyourfin/savefin/SavefinApplication.java
@@ -0,0 +1,13 @@
+package com.saveyourfin.savefin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavefinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavefinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saveyourfin/savefin/SavefinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saveyourfin.savefin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavefinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
